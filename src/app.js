94892c963ac85d034f4a815146b4d5107a98bc72
Pulkit Saxena <psaksena85@gmail.com>
2021-02-04 23:52:20 -0500

    sending response based on request

--- src/app.js
+++ src/app.js
@@ -1,6 +1,19 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
+	const url = req.url;
+	if (url === '/') {
+		res.setHeader('Content-Type', 'text/html');
+		res.write('<html>');
+		res.write('<head><title>Enter Message</title></head>');
+		res.write('<body>');
+		res.write(
+			'<form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form>'
+		);
+		res.write('</body>');
+		res.write('</html>');
+		return res.end();
+	}
 	res.setHeader('Content-Type', 'text/html');
 	res.write('<html>');
 	res.write('<head><title>First Page</title></head>');
