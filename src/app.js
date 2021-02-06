4bd79c63f2020772a8c59367080298e1ebeeaaaf
Pulkit Saxena <psaksena85@gmail.com>
2021-02-06 17:48:21 -0500

    redirecting request

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
 	const url = req.url;
+	const method = req.method;
+
 	if (url === '/') {
 		res.setHeader('Content-Type', 'text/html');
 		res.write('<html>');
@@ -12,6 +15,11 @@ const server = http.createServer((req, res) => {
 		);
 		res.write('</body>');
 		res.write('</html>');
+		return res.end();
+	} else if (url === '/message' && method === 'POST') {
+		fs.writeFileSync('/messages.txt', 'DUMMY');
+		res.statusCode = 302;
+		res.setHeader('Location', '/');
 		return res.end();
 	}
 	res.setHeader('Content-Type', 'text/html');
